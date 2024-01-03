9af6bf5d3144a0cbb7f28fba64bafad67abba1cf
zamfofex <zamfofex@twdb.moe>
2024-01-02 21:59:52 -0300

    update dependencies

--- scripts/clone-dependencies.sh
+++ scripts/clone-dependencies.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf emsdk libjxl
 
 git clone https://github.com/emscripten-core/emsdk
-(cd emsdk && git reset --hard dd8bbe5e8234f5db9692b5f107a14bfce3fcef34)
+(cd emsdk && git reset --hard 9347bc393b94a17b93450bbc98bc3f66cef2aeb0)
 
 git clone --recursive https://github.com/libjxl/libjxl
-(cd libjxl && git reset --hard 79946c2e12cc3e1e018a8411c75c32e02546baf8)
+(cd libjxl && git reset --hard c51cffd7587d27fdd73f0be856db3b730314f03c)
