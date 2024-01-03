8c84c0f1cabe62d62062508dcd8da83dcf9135d4
zamfofex <zamfofex@twdb.moe>
2024-01-02 22:00:19 -0300

    fix Guix build

--- scripts/guix-build.sh
+++ scripts/guix-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-guix shell -CFN coreutils bash gcc:lib cmake make python-wrapper pkgconf zlib zip git nss-certs tar xz bzip2 sed -- env C{,PLUS}_INCLUDE_PATH= scripts/build.sh
+guix shell -CFN -e '(list (@@ (gnu packages commencement) gcc) "lib")' coreutils bash cmake make python-wrapper pkgconf zlib zip git nss-certs tar xz bzip2 sed -- env C{,PLUS}_INCLUDE_PATH= scripts/build.sh
